17e98a68f924061a70b87fc848786cf83174a1b9
carraes <carraeshb@gmail.com>
2023-07-10 10:59:17 -0300

    refactor: expense route so it handle errors

--- src/app/api/expenses/route.ts
+++ src/app/api/expenses/route.ts
@@ -1,11 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { getAll } from '@/lib/getAll'
+import { getAllByDate } from '@/lib/services/getAll'
 
 export async function GET(req: NextRequest) {
   const { searchParams } = req.nextUrl
   const id = Number(searchParams.get('id'))
+  const startDate = searchParams.get('startDate')
+  const endDate = searchParams.get('endDate')
 
-  const expense = await getAll(id)
+  if (!id || !startDate || !endDate) {
+    throw new Error('Missing required parameters')
+  }
+
+  const expense = await getAllByDate(id, startDate, endDate)
 
   return NextResponse.json(expense)
 }
