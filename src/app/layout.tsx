6fc70e5a3613a62618e3ca59f08f002bf709fe21
carraes <carraeshb@gmail.com>
2023-07-10 17:54:34 -0300

    feat: React query provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import { ThemeProvider } from '@/components/ui/theme-provider'
 import Nav from '@/components/Nav'
+import { ReactQueryProvider } from './ReactQueryProvider'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Fluxr',
@@ -14,13 +20,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <main className="flex flex-col justify-center mx-auto w-full md:max-w-4xl border-x border-b rounded-sm p-2">
-            <Nav />
-            {children}
-          </main>
-        </ThemeProvider>
+      <body className={roboto.className}>
+        <ReactQueryProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <main className="flex flex-col justify-center mx-auto w-full md:max-w-4xl border-x border-b rounded-sm p-2">
+              <Nav />
+              {children}
+            </main>
+          </ThemeProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   )
