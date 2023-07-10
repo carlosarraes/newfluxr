32dd83c3355ecf4dc63867f5d638d24ffdaebfdc
carraes <carraeshb@gmail.com>
2023-07-10 10:59:35 -0300

    feat: changes to layout

--- src/app/relatorios/page.tsx
+++ src/app/relatorios/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <section className="p-2">
-      <h1 className="text-xl">Dashboard</h1>
+      <h1 className="text-xl">Relatorios</h1>
     </section>
   )
 }
